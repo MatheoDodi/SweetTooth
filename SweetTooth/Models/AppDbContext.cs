3635457104e7b8e56a3695d96505059ca64f4529
Matthew Dodi <matthew.dodi@gmail.com>
2019-03-20 22:39:55 -0700

    Type: feat - created database

--- SweetTooth/Models/AppDbContext.cs
+++ SweetTooth/Models/AppDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SweetTooth.Models
 {
-    public class AppDbContext
+    public class AppDbContext: DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Pie> Pies { get; set; } 
     }
 }
--- SweetTooth/Models/PieRepository.cs
+++ SweetTooth/Models/PieRepository.cs
@@ -5,7 +5,23 @@ using System.Threading.Tasks;
 
 namespace SweetTooth.Models
 {
-    public class PieRepository
+    public class PieRepository: IPieRepository
     {
+        private readonly AppDbContext _appDbContext;
+
+        public PieRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _appDbContext.Pies;
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            return _appDbContext.Pies.FirstOrDefault(p => p.PieId == pieId);
+        }
     }
 }
